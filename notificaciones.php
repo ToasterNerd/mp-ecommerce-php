d86a88f561b7a82e10cdbe485288b394944eb924
ToasterNerd <80899936+ToasterNerd@users.noreply.github.com>
2021-06-18 17:29:54 -0300

    Update notificaciones.php

--- notificaciones.php
+++ notificaciones.php
@@ -1,22 +1,58 @@
 <?php
-require 'vendor/autoload.php';
-require_once 'credenciales.php';
+public function webhook()
+{
+    MercadoPago\SDK::setAccessToken(ACCESS_TOKEN_MARKETPLACE);
+    MercadoPago\SDK::setIntegratorId(getenv('MP_DEV_CODE'));
+    $info = json_decode($this->input->raw_input_stream);
 
-MercadoPago\SDK::setAccessToken($access_token);
+    if (isset($info->type)) {
+        switch ($info->type) {
+            case 'mp-connect':
+                // Desvinculo de mi sistema cuando el usuario desautoriza la app desde su cuenta de Mercadopago.
+                if ($info->action == 'application.deauthorized') {
 
-        $payment = new MercadoPago\Payment.find_by_id($_POST["id"]);
+                    $data_update = array(
+                        'mp_access_token' => NULL,
+                        'mp_public_key' => NULL,
+                        'mp_refresh_token' => NULL,
+                        'mp_user_id' => NULL,
+                        'mp_expires_in' => NULL,
+                        'mp_status' => 0
+                    );
+
+                    $this->producers->update_mp_connect($data_update, $info->user_id);
+                    $this->output->set_status_header(200);
+                    return;
+                }
+
+                // Pueden tomar otra acción si el $info->action = 'application.authrized'
+            break;
+
+            case 'payment':
+                // Actualizo la información de pago recibida.
+                $or_collection_id = $info->data->id;
+                $info = MercadoPago\Payment::find_by_id($or_collection_id);
+                $or_number = $info->external_reference;
+
+                $data_update = array(
+                    'or_collection_status' => $info->status,
+                    'or_collection_status_detail' => $info->status_detail,
+                    'or_payment_type' => $info->payment_type_id,
+                    'or_payment_method' => $info->payment_method_id,
+                    'or_status' => gcfg($info->status,'or_status_collection_status')
+                );
+
+                $this->cart->update_ipn_order($data_update,$or_number);
+
+            break;
+
+            default:
+                $this->output->set_status_header(200);
+                return;
+            break;
+        }
+    }
+    $this->output->set_status_header(200);
+    return;
+}
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>notificaciones</title>
-</head>
-<body>
-    <?php echo $payment;
-     ?>
-</body>
-</html>
