0f56b11eb2e2ff702bfd50f239b22e7d565cb588
ToasterNerd <80899936+ToasterNerd@users.noreply.github.com>
2021-06-18 17:54:31 -0300

    Update notificaciones.php

--- notificaciones.php
+++ notificaciones.php
@@ -1,29 +1,22 @@
-<?php
 
+ <?php
 require 'vendor/autoload.php';
 require_once 'credenciales.php';
-
     MercadoPago\SDK::setAccessToken($access_token);
-    MercadoPago\SDK::setIntegratorId(getenv('dev_24c65fb163bf11ea96500242ac130004'));
-    $info = json_decode($this->input->raw_input_stream);
 
+    switch($_POST["type"]) {
+        case "payment":
+            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+            break;
+        case "plan":
+            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+            break;
+        case "subscription":
+            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+            break;
+        case "invoice":
+            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+            break;
+    }
 
-                // Actualizo la información de pago recibida.
-                $or_collection_id = $info->data->id;
-                $info = MercadoPago\Payment::find_by_id($or_collection_id);
-                $or_number = $info->external_reference;
-
-                $data_update = array(
-                    'or_collection_status' => $info->status,
-                    'or_collection_status_detail' => $info->status_detail,
-                    'or_payment_type' => $info->payment_type_id,
-                    'or_payment_method' => $info->payment_method_id,
-                    'or_status' => gcfg($info->status,'or_status_collection_status')
-                );
-
-  $this->cart->update_ipn_order($data_update,$or_number);
-
-
-    $this->output->set_status_header(200);
-    return;
 ?>
