cce0bb4e0123a80e645db79d89626aa01b90c822
ToasterNerd <80899936+ToasterNerd@users.noreply.github.com>
2021-06-20 09:24:27 -0300

    Update notificaciones.php

--- notificaciones.php
+++ notificaciones.php
@@ -20,6 +20,6 @@ require_once 'credenciales.php';
     }
 //ACA LE DARÍA EL USO QUE EL CLIENTE REQUERRIRÍA A LOS DATOS RECIBIDOS EN JSON. (ENVIAR A BBDD por ejemplo)
 http_response_code(200);
-echo json_encode($payment());
+//echo json_encode($payment());
 ?>
 
