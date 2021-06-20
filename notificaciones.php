c9f2e86bd07571dd62918dfaf956a1e6156f703c
ToasterNerd <80899936+ToasterNerd@users.noreply.github.com>
2021-06-20 09:31:55 -0300

    Update notificaciones.php

--- notificaciones.php
+++ notificaciones.php
@@ -3,19 +3,10 @@
 require 'vendor/autoload.php';
 require_once 'credenciales.php';
     MercadoPago\SDK::setAccessToken($access_token);
-
+ 
     switch($_POST["type"]) {
         case "payment":
-            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-            $payment = MercadoPago\Payment.find_by_id($_POST["live_mode"]);
-            $payment = MercadoPago\Payment.find_by_id($_POST["type"]);
-            $payment = MercadoPago\Payment.find_by_id($_POST["date_created"]);
-            $payment = MercadoPago\Payment.find_by_id($_POST["aplication_id"]);
-            $payment = MercadoPago\Payment.find_by_id($_POST["user_id"]);
-            $payment = MercadoPago\Payment.find_by_id($_POST["version"]);
-            $payment = MercadoPago\Payment.find_by_id($_POST["api_version"]);
-            $payment = MercadoPago\Payment.find_by_id($_POST["action"]);
-            $payment = MercadoPago\Payment.find_by_id($_POST["data"]["id"]);
+            $_POST = json_decode(file_get_contents('php://input'), true);
             break;
         case "plan":
             $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
