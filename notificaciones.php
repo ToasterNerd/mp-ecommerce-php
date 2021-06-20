58fcd77247cef482850dbe30df236bd9befc3885
ToasterNerd <80899936+ToasterNerd@users.noreply.github.com>
2021-06-20 09:33:55 -0300

    Update notificaciones.php

--- notificaciones.php
+++ notificaciones.php
@@ -6,22 +6,7 @@ require_once 'credenciales.php';
 MercadoPago\SDK::setAccessToken($access_token);
 
  $_POST = json_decode(file_get_contents('php://input'), true);
-  
-switch($_POST["type"]) {
-        case "payment":
-            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-            
-            break;
-        case "plan":
-            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
-            break;
-        case "subscription":
-            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
-            break;
-        case "invoice":
-            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
-            break;
-    }
+
 //ACA LE DARÍA EL USO QUE EL CLIENTE REQUERRIRÍA A LOS DATOS RECIBIDOS EN JSON. (ENVIAR A BBDD por ejemplo)
 http_response_code(200);
 //echo json_encode($payment());
